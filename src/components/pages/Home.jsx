25a50acd259a1f745479b9cb84620121ee6fec88
cassiolacerda <697508+cassiolacerda@users.noreply.github.com>
2022-09-29 09:25:34 -0300

    Popula dropdown de usuários com dados da API

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,15 +1,29 @@
+import React from "react";
+
 import logo from "../../images/logo.svg";
 
 export default function Home() {
+  const [users, setUsers] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("https://62c4e487abea8c085a7e022a.mockapi.io/users")
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data);
+      });
+  }, []);
+
   return (
     <div className="home center">
       <div className="home__logo">
         <img src={logo} className="responsive" alt="" />
       </div>
       <select className="home__select-users">
-        <option>User 1</option>
-        <option>User 2</option>
-        <option>User 3</option>
+        {users
+          .sort((a, b) => a.fn.localeCompare(b.fn))
+          .map((user) => (
+            <option key={user.id}>{`${user.fn} ${user.ln}`}</option>
+          ))}
       </select>
       <button className="button-primary">Entrar</button>
     </div>
