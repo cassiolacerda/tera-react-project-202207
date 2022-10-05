6604d41276b2048bcc716dc14f85d0eeeca8f5f5
cassiolacerda <697508+cassiolacerda@users.noreply.github.com>
2022-10-05 20:07:30 -0300

    Adiciona página de formulário criar post

--- src/components/pages/UserPostForm.jsx
+++ src/components/pages/UserPostForm.jsx
@@ -1,9 +1,33 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 import Default from "../templates/Default";
+import AppLoading from "../organisms/AppLoading";
 
 export default function UserPostForm() {
-  return (
+  const { userId } = useParams();
+
+  const [title, setTitle] = React.useState("");
+  const [content, setContent] = React.useState("");
+  const [isLoading, setIsLoading] = React.useState(false);
+
+  const handleConfirmClick = () => {
+    setIsLoading(true);
+
+    fetch(`https://62c4e487abea8c085a7e022a.mockapi.io/users/${userId}/posts`, {
+      method: "POST",
+      headers: { "Content-type": "application/json; charset=UTF-8" },
+      body: JSON.stringify({ title, content }),
+    }).then(() => {
+      setTitle("");
+      setContent("");
+      setIsLoading(false);
+    });
+  };
+
+  return isLoading ? (
+    <AppLoading />
+  ) : (
     <Default>
       <div className="create-post">
         <h1>Criar</h1>
@@ -11,13 +35,26 @@ export default function UserPostForm() {
         <form className="create-post__form">
           <div className="create-post__form-name">
             <label htmlFor="name">Título</label>
-            <input type="text" id="name" name="title" />
+            <input
+              type="text"
+              id="name"
+              name="title"
+              value={title}
+              onChange={(event) => setTitle(event.target.value)}
+            />
           </div>
           <div className="create-post__form-content">
             <label htmlFor="content">Conteúdo</label>
-            <textarea name="content" id="content"></textarea>
+            <textarea
+              name="content"
+              id="content"
+              value={content}
+              onChange={(event) => setContent(event.target.value)}
+            ></textarea>
           </div>
-          <button className="button-primary">Salvar</button>
+          <button onClick={handleConfirmClick} className="button-primary">
+            Salvar
+          </button>
         </form>
       </div>
     </Default>
